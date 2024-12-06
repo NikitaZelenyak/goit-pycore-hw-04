3028b6e1f9d624c7b597f7bdc78aeee246a93f9e
Nikita Zeleniak <zeleniak.nikita@gmail.com>
2024-12-06 13:53:48 -0500

    updated code

--- 03/src/helpers/list_directory_contents.py
+++ 03/src/helpers/list_directory_contents.py
@@ -7,10 +7,12 @@ def list_directory_contents(p):
         print("Path is not exist")
     else:
         for el in path.rglob("*"):
+            pathList = str(el).split("/")
             if el.is_dir():
-              print(f"{Fore.BLUE} Dir: {el} {Fore.RESET}")
+             
+              print(f"{Fore.BLUE}{len(pathList) * "   "}{pathList[-1]}{Fore.RESET}")
             elif el.is_file():
-                print(f"{Fore.YELLOW} File: {el} {Fore.RESET}")
+                print(f"{Fore.YELLOW}{len(pathList) * "   "}{pathList[-1]} {Fore.RESET}")
             else:
-                print(f"{Fore.RED} {el} - is not file or dir {Fore.RESET}")
+                print(f"{Fore.RED} {pathList[-1]} - is not file or dir {Fore.RESET}")
 
